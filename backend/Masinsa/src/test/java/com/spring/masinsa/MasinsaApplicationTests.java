32bd5cfd7c3748a53bcaffc3393c643fb7656c6f
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-12-08 11:20:25 +0900

    MVC 패턴 기본 세팅

---
+++ backend/Masinsa/src/test/java/com/spring/masinsa/MasinsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.masinsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasinsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
