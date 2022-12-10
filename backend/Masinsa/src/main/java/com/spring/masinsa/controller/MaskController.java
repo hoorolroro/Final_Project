aefc173cc3dacc9c3fb1b1bfc2ae17bb5f6a2a20
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-12-11 01:45:27 +0900

    Swagger 간단 설명 추가

--- backend/Masinsa/src/main/java/com/spring/masinsa/controller/MaskController.java
+++ backend/Masinsa/src/main/java/com/spring/masinsa/controller/MaskController.java
@@ -14,6 +14,8 @@ import com.spring.masinsa.dto.ImageDTO;
 import com.spring.masinsa.dto.MaskDTO;
 import com.spring.masinsa.service.MaskServiceImpl;
 
+import io.swagger.annotations.ApiOperation;
+
 @CrossOrigin({"*"})
 @RestController
 public class MaskController {
@@ -21,19 +23,19 @@ public class MaskController {
 	@Autowired
 	MaskServiceImpl maskService;
 
-	// 5번 - maskID를 통해 마스크 정보 조회
+	@ApiOperation(value = "5번 - maskId를 통해 마스크 정보 조회")
 	@GetMapping("/mask")
 	public MaskDTO getMask(@RequestParam Long maskId) {
 		return maskService.getMask(maskId);
 	}
 	
-	// 12번 - maskId를 통해 마스크 상세 이미지 조회
+	@ApiOperation(value = "12번 - maskId를 통해 마스크 상세 이미지 조회")
 	@GetMapping("/mask/image")
 	public List<String> getMaskDetailImage(@RequestParam Long maskId) {
 		return maskService.getAllImages(maskId);
 	}
-	
-	// 9번 - kf, size, shape를 이용하여 검색결과 조회 (필터링)
+	 
+	@ApiOperation(value = "9번 - kf, size, shape를 이용하여 검색결과 조회 (필터링)")
 	@GetMapping("/mask/filter")
 	public List<MaskDTO> getMaskByFilter(@RequestParam(required = false) String kf, 
 									  @RequestParam(required = false) String size, 
@@ -41,31 +43,31 @@ public class MaskController {
 		return maskService.getAllMask(kf, size, shape);
 	}
 	
-	// maskId를 통해 해당 마스크의 모든 이미지 조회
+	@ApiOperation(value = "maskId를 통해 해당 마스크의 모든 이미지 조회")
 	@GetMapping("/mask/image2")
 	public List<ImageDTO> getMaskDetailImages(@RequestParam Long maskId) {
 		return maskService.getAllImagesOb(maskId);
 	}
 	
-	// 10번 - standard를 통해 마스크 해당 기준으로 정렬 (해결 필요...)
+	@ApiOperation(value = "10번 - standard를 통해 마스크 해당 기준으로 정렬 (해결 필요...)")
 	@GetMapping("/mask/sort")
 	public List<MaskDTO> getSortedMasks(@RequestParam String standard) {
 		return maskService.getSortedMasks(standard);
 	}
 	
-	// 6번 - maskId와 soldout을 통해 마스크 품절 여부 수정
+	@ApiOperation(value = "6번 - maskId와 soldout을 통해 마스크 품절 여부 수정")
 	@PutMapping("/mask/soldout")
 	public void updateSoldout(@RequestParam Long maskId, @RequestParam String soldout) {
 		maskService.updateSoldout(maskId, soldout);
 	}
 	
-	// 7번 - maskId를 통해 해당 마스크의 클릭수 1 추가
+	@ApiOperation(value = "7번 - maskId를 통해 해당 마스크의 클릭수 1 추가")
 	@PutMapping("/mask/click")
 	public void updateClick(@RequestParam Long maskId) {
 		maskService.updateClick(maskId);
 	}
 	
-	// 8번 - maskId를 통해 마스크 정보 삭제
+	@ApiOperation(value = "8번 - maskId를 통해 마스크 정보 삭제")
 	@DeleteMapping("/mask")
 	public void deleteMask(@RequestParam Long maskId) {
 		maskService.deleteMask(maskId);
