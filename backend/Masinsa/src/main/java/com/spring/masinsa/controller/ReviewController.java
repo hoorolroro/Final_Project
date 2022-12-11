3b59dc0c5b818ac6c3fb7645e2c8b388e1646b17
domini04 <107534459+domini04@users.noreply.github.com>
2022-12-11 21:43:37 +0900

    api 14번 구현
    - reviewId로 검색하던 기존의 메소드에서 maskId로 검색하도록 수정
    - pageable을 사용하여 구현 -> page, size 받도록
    - reviewType을 받도록 하여 네이버리뷰/회원리뷰 검색 통합

--- backend/Masinsa/src/main/java/com/spring/masinsa/controller/ReviewController.java
+++ backend/Masinsa/src/main/java/com/spring/masinsa/controller/ReviewController.java
@@ -3,6 +3,10 @@ package com.spring.masinsa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,15 +23,18 @@ public class ReviewController {
   @Autowired 
   ReviewServiceImpl reviewService;
 
+  //AnalysisInfoController.java에서 분석정보를 만들기 위해 리뷰갯수를 확인하는 메소드
   @GetMapping("/review/count")
   public int countByMaskId(@RequestParam Long maskId) {
     return reviewService.countByMaskId(maskId);
   }
   
   @GetMapping("/review")
-  public ResponseEntity<List<ReviewDTO>> findByMaskId(@RequestParam Long maskId) {
-    List<ReviewDTO> reviewList = reviewService.findByMaskId(maskId);
-    return ResponseEntity.ok(reviewList);
+  public ResponseEntity<List<ReviewDTO>> findByMaskIdAndReviewType(@RequestParam Long maskId,
+      @PageableDefault(size = 10, page = 1) Pageable pageable, 
+      @RequestParam String reviewType) {
+    pageable = PageRequest.of(pageable.getPageNumber() - 1, pageable.getPageSize()); //page는 0부터 시작 -> front에서 1부터 시작하므로 -1해줘야함
+    return ResponseEntity.ok(reviewService.findByMaskIdAndReviewType(maskId, pageable, reviewType));
   }
 
 }
--- backend/Masinsa/src/main/java/com/spring/masinsa/repository/ReviewRepository.java
+++ backend/Masinsa/src/main/java/com/spring/masinsa/repository/ReviewRepository.java
@@ -2,10 +2,12 @@ package com.spring.masinsa.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.spring.masinsa.entity.Review;
+import com.spring.masinsa.entity.ReviewType;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
@@ -13,7 +15,9 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
   @Query(value = "select count(*) from review where mask_id = ?1 and review_type = 'naver'", nativeQuery = true)
   public int countByMaskId(Long maskId);
 
-  @Query(value = "select * from review where mask_id = ?1", nativeQuery = true)
-public List<Review> findByMaskId(Long maskId);
+// find by maskId and review type
+public List<Review> findByMaskIdAndReviewType(Long maskId, ReviewType reviewType, Pageable pageable);
+// public List<Review> findByMaskId(Long maskId, Pageable pageable); //Slice : 페이징 처리를 위한 객체
+
   
 }
