2fb4ebf6f80c4899f90d9369288138663d5bf133
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-12-13 20:03:43 +0900

    wishList 관련 메소드 80% 구현

--- backend/Masinsa/src/main/java/com/spring/masinsa/repository/WishListRepository.java
+++ backend/Masinsa/src/main/java/com/spring/masinsa/repository/WishListRepository.java
@@ -1,9 +1,12 @@
 package com.spring.masinsa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.spring.masinsa.entity.WishList;
 
 public interface WishListRepository extends JpaRepository<WishList, Long> {
-	public WishList findWishListByMemberId(Long memberId);
+	public List<WishList> findWishListByMemberId(Long memberId);
+	public WishList findWishListById(Long WishListId);
 }
--- backend/Masinsa/src/main/java/com/spring/masinsa/service/WishListServiceImpl.java
+++ backend/Masinsa/src/main/java/com/spring/masinsa/service/WishListServiceImpl.java
@@ -1,5 +1,50 @@
 package com.spring.masinsa.service;
 
-public class WishListServiceImpl implements WishListService {
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.masinsa.dto.WishListDTO;
+import com.spring.masinsa.entity.WishList;
+import com.spring.masinsa.mapper.WishListMapper;
+import com.spring.masinsa.repository.WishListRepository;
+
+@Service
+public class WishListServiceImpl implements WishListService {
+	
+	@Autowired
+	WishListRepository wishListRepo;
+	
+	@Autowired
+	WishListMapper wishListMapper;
+	
+	public WishListDTO addWishList(Long maskId, Long memberId) {
+		Long wishListId = wishListMapper.addWishList(maskId, memberId);
+		System.out.println(wishListId);
+		WishList wishList = wishListRepo.findWishListById(wishListId);
+		WishListDTO wishListDTO = WishList.entityToDTO(wishList);
+		return wishListDTO;
+	}
+	
+	public List<WishListDTO> getAllWishList(Long memberId) {
+		List<WishList> wishList = wishListRepo.findWishListByMemberId(memberId);
+		List<WishListDTO> wishListDTO = wishList.stream()
+										  .map(wish -> WishList.entityToDTO(wish))
+										  .collect(Collectors.toList());
+		return wishListDTO;
+	}
+	
+	public WishListDTO deleteWishList(Long wishListId) {
+		WishList wishList = wishListRepo.findWishListById(wishListId);
+		if(wishList != null) {
+			wishList.deleteWishList();
+			wishListRepo.save(wishList);
+			WishListDTO wishListDTO = WishList.entityToDTO(wishList);
+			return wishListDTO;
+		}
+		return null;
+	}
+	
 }
