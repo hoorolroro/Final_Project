5c2d3484563ee29e78a4190e1043329aa3102620
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-12-26 15:46:54 +0900

    사용 안하는 코드 삭제 및 메소드 반환값 일부 수정

--- backend/Masinsa/src/main/java/com/spring/masinsa/repository/ReviewRepository.java
+++ backend/Masinsa/src/main/java/com/spring/masinsa/repository/ReviewRepository.java
@@ -10,13 +10,9 @@ import com.spring.masinsa.entity.ReviewType;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-  //maskId를 통해 해당 review의 갯수를 count해서 리턴 -> 분석정보를 만들지 말지 결정하기 위해
   public int countByMaskIdAndReviewType(Long maskId, ReviewType reviewType);
-
-// find by maskId and review type
-  public List<Review> findByMaskIdAndReviewType(Long maskId, ReviewType reviewType, Pageable pageable);
-// public List<Review> findByMaskId(Long maskId, Pageable pageable); //Slice : 페이징 처리를 위한 객체
-
+  public List<Review> findByMaskIdAndReviewType(Long maskId, 
+		  ReviewType reviewType, Pageable pageable);
   public Review findReviewById(Long reviewId);
   public Review findReviewByMemberIdAndMaskId(Long memberId, Long maskId);
   
